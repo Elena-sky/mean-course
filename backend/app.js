8bdcfb3048ef633e7a758ed60844343182f75567
Alena Soroka <alena.soroka@ab-soft.net>
2020-04-20 19:18:08 +0300

    Fetching Initial Posts

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("111");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send("Hello from express!");
+app.use('/api/posts',(req, res, next) => {
+  const posts = [
+    {
+      id: 'dfg3427sdg',
+      title: "First server-side post",
+      content: "This is commit from the server"
+    },
+    {
+      id: 'dfg34rwrsdg',
+      title: "Second server-side post",
+      content: "This is commit from the server"
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched seccesfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
