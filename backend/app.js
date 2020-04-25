e20e49520cca2028610d9de8a3990d03299ba8ca
Alena Soroka <alena.soroka@ab-soft.net>
2020-04-26 00:44:58 +0300

    Save edited post to DB

--- backend/app.js
+++ backend/app.js
@@ -45,8 +45,9 @@ app.post("/api/posts", (req,res, next) => {
 
 app.put("/api/posts/:id", (req,res, next) => {
   const post = new Post({
-    title: req.title,
-    content: req.content
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
   });
   Post.updateOne({_id: req.params.id},  post).then(result => {
     res.status(200).json({
@@ -63,6 +64,16 @@ app.get('/api/posts',(req, res, next) => {
         posts: documents
       });
     });
+});
+
+app.get("/api/posts/:id", (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post)
+    } else {
+      res.status(404).json({message: "Post not found!"})
+    }
+  })
 });
 
 app.delete("/api/posts/:id", (req, res, next) => {
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -26,7 +26,9 @@ export class PostCreateComponent implements OnInit{
       if (paramMac.has('postId')) {
         this.mode = 'edit';
         this.postId = paramMac.get('postId');
-        this.post = this.postsService.getPost(this.postId);
+        this.postsService.getPost(this.postId).subscribe(postData => {
+          this.post = {id: postData._id, title: postData.title, content: postData.content};
+        });
       } else {
         this.mode = 'create';
         this.postId = null;
